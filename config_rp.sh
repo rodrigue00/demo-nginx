566a3afee7cae227afd64b879475209fa3f3002d
Francois Roland <froland@froland.be>
2021-01-08 12:05:57 +0100

    Use sudo.

--- config_rp.sh
+++ config_rp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo cp ./nodejs_rp.conf /etc/nginx/default.d
-chown root:root /etc/nginx/default.d/nodejs_rp.conf
-chmod 0644 /etc/nginx/default.d/nodejs_rp.conf
+sudo chown root:root /etc/nginx/default.d/nodejs_rp.conf
+sudo chmod 0644 /etc/nginx/default.d/nodejs_rp.conf
 sudo nginx -t
 sudo nginx -s reload
